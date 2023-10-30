d43f256667d3e1fa86aa9c215fa1e7ba9ec15de0
INFOPULSE\Tetiana.Dibrova <te.dibrova@gmail.com>
2023-10-30 15:26:08 +0200

    SWD_Dibrova_HW1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD_Dibrova1
+{
+    internal class Person
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Tania";
+            string surname = "Dibrova";
+            int age = 32;
+            Console.WriteLine($"I am {age} years old, my name is {name} {surname}");
+
+            int x = 20;
+            int y = 30;
+            sumMyNumbers(x, y);
+
+        }
+        static void sumMyNumbers(int x, int y)
+        {
+
+            int result = x + y;
+
+            Console.WriteLine($"My sum is {result}");
+        }
+    }
+}
+
