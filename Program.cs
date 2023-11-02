61fff61d3415e438e140503c9c654ec39df735b4
INFOPULSE\Tetiana.Dibrova <te.dibrova@gmail.com>
2023-11-02 20:23:37 +0200

    SWD_Dibrova_HW5

--- Program.cs
+++ Program.cs
@@ -6,34 +6,27 @@ namespace SWD_Dibrova1
     {
         static void Main(string[] args)
         {
-            // HW1
+
+            int x = 25;
+            int y = 16;
+                     
+            СountSquareRoot(x, y);
+
+            // please see SquareRoot class 
+            //var squareRoot = new сountSquareRoot(9, 15);
+
+
+
+
+
+            // HW 3.1
             /*
                 int x = 20;
                 int y = 30;
                 PrintSumNumb(x, y);
             */
 
-            //HW 2.1
-            var person = new Person("Name", "Surname", 99);
-            person.PrintPerson();
-            
-
-            //HW 2.2
-            //признаюсь чесно, що рішення нагуглила =) =( 
-            
-            int a = 1;
-            int b = 2;
-            (a, b) = (b, a);
-
-
-            Console.WriteLine($"First a: {a} First b: {b}");
-            Console.WriteLine($"Second a: {a} Second b: {b}");
-
-            Console.ReadLine();
-        }
-
-            // HW1
-
+            // HW 3.2
             /*
             static void PrintSumNumb(int x, int y)
             {
@@ -43,6 +36,49 @@ namespace SWD_Dibrova1
                 Console.WriteLine($"My sum is {result}");
             }
             */
+
+            // HW 4.1
+            /*
+             var person = new Person("Name", "Surname", 99);
+             person.PrintPerson
+            */
+
+            // HW 4.2
+            // признаюсь чесно, що рішення нагуглила =) =(             
+            /*
+            int a = 1;
+            int b = 2;
+            (a, b) = (b, a);
+
+            Console.WriteLine($"First a: {a} First b: {b}");
+            Console.WriteLine($"Second a: {a} Second b: {b}");
+            */
+
+
+
+
+            Console.ReadLine();
+        }
+
+        // HW 5
+        static void СountSquareRoot(int x, int y)
+        {
+
+            if (x < y)
+
+            {
+
+                Console.WriteLine(Math.Sqrt(y));
+            }
+            else
+            {
+
+                Console.WriteLine(Math.Sqrt(x));
+            }
+        }
+
+
+
     }
 }
 
---
+++ SquareRoot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SWD_Dibrova1
+{
+    public class SquareRoot
+    {
+
+
+
+        //public void СountSquareRoot(int x, int y)
+        //{
+
+        //    if (x < y)
+
+        //    {
+
+        //        Console.WriteLine(Math.Sqrt(y));
+        //    }
+
+        //    else
+        //    {
+
+        //        Console.WriteLine(Math.Sqrt(x));
+        //    }
+        //}
+
+
+
+
+    }
+
+
+}
